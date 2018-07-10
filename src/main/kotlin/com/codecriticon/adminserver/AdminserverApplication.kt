dfab8b3ccbac37327da81897c454ed327d907fd0
avidal-stratio <avidal@stratio.com>
2018-07-10 23:03:55 +0200

    Several Admin configurations
    - Add enable adminserver annotation
    - Configure server port
    - Basic admin configuration

--- src/main/kotlin/com/codecriticon/adminserver/AdminserverApplication.kt
+++ src/main/kotlin/com/codecriticon/adminserver/AdminserverApplication.kt
@@ -1,9 +1,11 @@
 package com.codecriticon.adminserver
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@EnableAdminServer
 class AdminserverApplication
 
 fun main(args: Array<String>) {
